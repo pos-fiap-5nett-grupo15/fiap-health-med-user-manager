76a421de595ccf830ce73fb49c4d07cb113c3f12
Evandro <evandroprates15@gmail.com>
2025-03-24 13:09:55 -0300

    retirado WeatherForecast

--- src/Fiap.Health.Med.User.Manager.Api/Program.cs
+++ src/Fiap.Health.Med.User.Manager.Api/Program.cs
@@ -34,8 +34,3 @@ internal class Program
         app.Run();
     }
 }
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
